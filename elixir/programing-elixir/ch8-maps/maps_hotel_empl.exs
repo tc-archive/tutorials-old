f2c8af1c5f4fe3d67674241a77ea21b6e84fe8cb
Temple Cloud <temple.cloud@gmail.com>
2014-07-29 15:30:08 +0100

    Forgot to add the file.

---
+++ elixir/programing-elixir/ch8-maps/maps_hotel_empl.exs
@@ -0,0 +1,21 @@
+defmodule HotelRoom do
+
+  # ***********************************************************************************************
+  @doc """
+  Hotel example using maps.
+  """
+
+	def book(%{name: name, height: height})
+		when height > 1.9 do
+		IO.puts "Need extra long bed for #{name}"
+	end
+
+	def book(%{name: name, height: height})
+		when height < 1.3 do
+		IO.puts "Need low shower controls for #{name}"
+	end
+
+	def book(person) do
+	IO.puts "Need regular bed for #{person.name}"
+	end
+end
