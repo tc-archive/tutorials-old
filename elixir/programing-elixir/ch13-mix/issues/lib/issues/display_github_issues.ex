59368075dcfcebefd46ab0c2c0710449024b94dc
TempleCloud <tim.langford@gmail.com>
2014-08-09 14:41:41 +0100

    Working on table header function.

--- elixir/programing-elixir/ch13-mix/issues/lib/issues/display_github_issues.ex
+++ elixir/programing-elixir/ch13-mix/issues/lib/issues/display_github_issues.ex
@@ -15,28 +15,21 @@ defmodule Issues.DisplayGithubIssues do
   def display(list_of_hash_dicts) do 
 
     fmtr = Enum.reduce(list_of_hash_dicts, {0, 0, 0}, &build_fmtr(&1, &2))
+    display_header(fmtr)
 
-    
-    
     Enum.each(list_of_hash_dicts, fn hash_dict -> display_hash_dict(hash_dict) end)
 
   end
 
-  def build_fmtr(list_of_hash_dicts) do 
-    Enum.reduce(list_of_hash_dicts, {0, 0, 0}, &build_fmtr(&1, &2))
-  end
 
-  def build_fmtr(hash_dict, {mx_rec_num_wth, mx_crtd_at_wth, mx_title_wth}) do 
+#  #  | created_at           | title
+# ----+----------------------+-----------------------------------------
+# 889 | 2013-03-16T22:03:13Z | MIX_PATH environment variable (of sorts)
+# 892 | 2013-03-20T19:22:07Z | Enhanced mix test --cover
+# 893 | 2013-03-21T06:23:00Z | mix test time reports
+# 898 | 2013-03-23T19:19:08Z | Add mix compile --warnings-as-errors
 
-    {rec_num, crtd_at, title} = extract(hash_dict)
-
-    new_mx_rec_num_wth = max(mx_rec_num_wth, String.length(rec_num))
-    new_mx_crtd_at_wth = max(mx_crtd_at_wth, String.length(crtd_at))
-    new_mx_title_wth = max(mx_title_wth, String.length(title))
-
-    {new_mx_rec_num_wth, new_mx_crtd_at_wth, new_mx_title_wth}
-  end
-
+  # ***************************************************************************
 
   defp display_hash_dict(hash_dict) do 
 
@@ -47,10 +40,44 @@ defmodule Issues.DisplayGithubIssues do
   end
 
 
+  def display_header({col1, col2, col3}) do 
+
+    line = <<>>
+    line = for x <- 0..col1, into: line, do: "-" 
+    line = line <> "+" 
+    line = for x <- 0..col2, into: line, do: "-" 
+    line = line <> "+" 
+    line = for x <- 0..col3, into: line, do: "-" 
+
+    # IO.puts line
+
+  end
+
   defp display_rec({rec_num, crtd_at, title}) do 
     IO.puts "#{rec_num}, #{crtd_at}, #{title}"
   end
 
+
+  # ***************************************************************************
+  
+  def build_fmtr(list_of_hash_dicts) do 
+    Enum.reduce(list_of_hash_dicts, {0, 0, 0}, &build_fmtr(&1, &2))
+  end
+
+  def build_fmtr(hash_dict, {col1_mx, col2_mx, col3_mx}) do 
+
+    {rec_num, crtd_at, title} = extract(hash_dict)
+
+    new_col1_mx = max(col1_mx, String.length(rec_num))
+    new_col2_mx = max(col2_mx, String.length(crtd_at))
+    new_col3_mx = max(col3_mx, String.length(title))
+
+    {new_col1_mx, new_col2_mx, new_col3_mx}
+
+  end
+
+
+  # ***************************************************************************
 
   def extract(record_dict) do
     rec_num = "#{HashDict.get(record_dict, "number")}"
