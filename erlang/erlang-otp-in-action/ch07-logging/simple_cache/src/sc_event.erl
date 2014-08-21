eab58ca9b9c15578d4c64163c9a207503d0c483e
TempleCloud <tim.langford@gmail.com>
2014-08-21 20:23:01 +0100

    Working on ch7 of erlang otp in action - simple_cache event stream.

--- erlang/erlang-otp-in-action/ch07-logging/simple_cache/src/sc_event.erl
+++ erlang/erlang-otp-in-action/ch07-logging/simple_cache/src/sc_event.erl
@@ -11,12 +11,20 @@
 %%% OTP GenEvent Behaviour
 %%%============================================================================
 
--behaviour(gen_event).
+% -behaviour(gen_event).
+
+
+%%%============================================================================
+%%% Public API
+%%%============================================================================
 
 -export([
   start_link/0,
   add_handler/2,
-  delete_handler/2,
+  delete_handler/2
+]).
+
+-export([
   lookup/1,
   create/2,
   replace/2,
@@ -36,27 +44,33 @@
 %%%============================================================================
 
 start_link() ->
+  % Hide gen_event start function.
   gen_event:start_link({local, ?SERVER}).
 
 add_handler(Handler, Args) ->
+  % Hides gen_event handler registration.
   gen_event:add_handler(?SERVER, Handler, Args).
 
 delete_handler(Handler, Args) ->
+  % Hides gen_event handler de-registration.
   gen_event:delete_handler(?SERVER, Handler, Args).
 
 lookup(Key) ->
+  % API function.
   gen_event:notify(?SERVER, {lookup, Key}).
 
 create(Key, Value) ->
+  % API function
   gen_event:notify(?SERVER, {create, {Key, Value}}).
 
 replace(Key, Value) ->
+  % API function.
   gen_event:notify(?SERVER, {replace, {Key, Value}}).
 
 delete(Key) ->
+  % API function.
   gen_event:notify(?SERVER, {delete, Key}).
 
 
-% B Hides gen_event start function
-% C Hides gen_event handler registration
-% D API functions
+
+
