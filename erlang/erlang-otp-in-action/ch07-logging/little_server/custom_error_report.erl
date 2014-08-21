30a611948e573e1d4f3f4c67603b3188a2b9207d
TempleCloud <tim.langford@gmail.com>
2014-08-21 18:25:32 +0100

    Working on ch7 of erlang otp in action.

---
+++ erlang/erlang-otp-in-action/ch07-logging/little_server/custom_error_report.erl
@@ -0,0 +1,86 @@
+%%%============================================================================
+%%% @doc 
+%%% A barebones implementation of a gen_event behaviour for the error logger.
+%%% @end
+%%%============================================================================
+
+-module(custom_error_report).
+
+%% *** There’s no stopping ***
+%%
+%% The callback functions of a 'gen_server' can return a stop value, telling the 
+%% server process to shut down. A 'gen_event' callback can’t do that (because 
+%% other registered handlers may not be happy if you kill the entire service). 
+%%
+%% Instead, the callback can return a 'remove_handler' value, which causes the 
+%% 'gen_event' process to remove that handler, calling its terminate callback 
+%% function as a last favor.
+%%
+
+%%%============================================================================
+%%% OTP GenEvent Behaviour
+%%%============================================================================
+
+-behaviour(gen_event).
+
+-export([
+  init/1, 
+  handle_event/2, 
+  handle_call/2,
+  handle_info/2, 
+  terminate/2, 
+  code_change/3
+]).
+
+
+%%%============================================================================
+%%% Public API
+%%%============================================================================
+
+-export([register_with_logger/0]).
+
+
+%%%============================================================================
+%%% Records
+%%%============================================================================
+
+-record(state, {}).
+
+%%%============================================================================
+%%% Public API Implementation
+%%%============================================================================
+
+% Register this customer GenEvent process with the 'error_logger'
+register_with_logger() ->
+  % Add this GenEvent process as 'report handler' with the 'error_logger'.
+  error_logger:add_report_handler(?MODULE).
+
+init([]) ->
+  {ok, #state{}}.
+
+% Default 'do nothing' implementation.
+handle_event(_Event, State) ->
+  {ok, State}.
+
+% Default 'do nothing' implementation.
+handle_call(_Request, State) ->
+  Reply = ok,
+  {ok, Reply, State}.
+
+% Default 'do nothing' implementation.
+handle_info(_Info, State) ->
+  {ok, State}.
+
+
+% Default 'do nothing' implementation.
+terminate(_Reason, _State) ->
+  ok.
+
+% Default 'do nothing' implementation.
+code_change(_OldVsn, State, _Extra) ->
+  {ok, State}.
+
+
+
+
+
