a280c0654af1facf5fe76961da4a4a03b1e12e1e
TempleCloud <tim.langford@gmail.com>
2014-08-21 17:32:10 +0100

    Working on ch7 of erlang otp in action.

--- erlang/erlang-otp-in-action/ch07-logging/little_server/die_please_non_otp.erl
+++ erlang/erlang-otp-in-action/ch07-logging/little_server/die_please_non_otp.erl
@@ -3,7 +3,7 @@
 %%% A non OTP version of the 'die_please' OTP GenServer.
 %%% @end
 %%%============================================================================
--module(die_please2).
+-module(die_please_non_otp).
 
 
 %%%============================================================================
@@ -25,6 +25,15 @@
 %%%============================================================================
 
 go() ->
-    %% just sleep for a while, then crash
-    timer:sleep(?SLEEP_TIME),
-    i_really_want_to_die = right_now.
+  %% just sleep for a while, then crash
+  timer:sleep(?SLEEP_TIME),
+  i_really_want_to_die = right_now.
+
+
+
+% 9> die_please_non_otp:go.
+% * 1: illegal expression
+% 10> die_please_non_otp:go().
+% ** exception error: no match of right hand side value right_now
+%      in function  die_please_non_otp:go/0 (die_please_non_otp.erl, line 30)
+% 11>
