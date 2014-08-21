e390be07ae5f1c4a0c827805a79f679cd200cdce
TempleCloud <tim.langford@gmail.com>
2014-08-21 13:34:34 +0100

    Working on ch6 of erlang otp in action.

--- erlang/erlang-otp-in-action/ch06-simple-cache/simple_cache/src/simple_cache.erl
+++ erlang/erlang-otp-in-action/ch06-simple-cache/simple_cache/src/simple_cache.erl
@@ -26,8 +26,13 @@ The convention for application-level API modules is to give them the same name a
 
 -export([insert/2, lookup/1, delete/1]).
 
+%% @doc
 %% Takes a key and a value and stores the pair in the cache with the 
 %% default LeaseTime.
+%% 
+%% Return {noreply, State} tuple (if 'replacing' a value), or, the atom 
+%% true (if 'inserting' a new Value).
+%% @end
 insert(Key, Value) ->
   case sc_store:lookup(Key) of
     {ok, Pid} ->
@@ -37,8 +42,14 @@ insert(Key, Value) ->
       sc_store:insert(Key, Pid)
   end.
 
+
+%% @doc
 %% Takes a key and a value and stores the pair in the cache with the 
-%% specified LeaseTime.
+%% specified LeaseTime. 
+%% 
+%% Return {noreply, State} tuple (if 'replacing' a value), or, the atom 
+%% true (if 'inserting' a new Value).
+%% @end
 insert(Key, Value, LeaseTime) ->
   case sc_store:lookup(Key) of
     {ok, Pid} ->
@@ -49,9 +60,16 @@ insert(Key, Value, LeaseTime) ->
   end.
 
 
+%% @doc
+%% Takes a key and returns the associated Value ins an {ok, Value} 
+%% tuple; or returns an  {error, not_found} tuple.
+%% @end
 lookup(Key) ->
+  % Using a try expression like this can be useful whenever you have a 
+  % sequence of things that must be done in order and the result should 
+  % be the same if any of the steps fails.
   try
-     {ok, Pid} = sc_store:lookup(Key),
+    {ok, Pid} = sc_store:lookup(Key),
     {ok, Value} = sc_element:fetch(Pid),
     {ok, Value}
   catch
@@ -59,7 +77,12 @@ lookup(Key) ->
     {error, not_found}
   end.
 
-
+%% @doc
+%% Takes a key and terminates the associated process holding the Value state.
+%%
+%% Returns a {stop, normal, State} tuple if an existing process was 
+%% terminated; or the atom ok if it did not exists.
+%% @end
 delete(Key) ->
   case sc_store:lookup(Key) of
     {ok, Pid} ->
