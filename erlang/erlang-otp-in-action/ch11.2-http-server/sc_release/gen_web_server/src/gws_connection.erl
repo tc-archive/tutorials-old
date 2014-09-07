4700b82c9fb965f38fac8f73c1e3fa5de25017f5
TempleCloud <tim.langford@gmail.com>
2014-09-07 12:49:28 +0100

    Working on ch11 manning otp in action. OTP implementation. Added basic 'gen_web_server' behaviour.

---
+++ erlang/erlang-otp-in-action/ch11.2-http-server/sc_release/gen_web_server/src/gws_connection.erl
@@ -0,0 +1,7 @@
+%%%============================================================================
+%%% @doc 
+%%% The supervisor implemented by the gws_connection_sup module will be a 
+%%% 'simple-one-for-one' supervisor, used as a factory for handler processes.
+%%% @end
+%%%============================================================================
+-module(gws_connection.erl).
