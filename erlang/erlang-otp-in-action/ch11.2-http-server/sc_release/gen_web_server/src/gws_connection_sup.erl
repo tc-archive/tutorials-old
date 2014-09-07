d6f5c16ea3d1ec48c5ab22bca7909ea4a282b0fa
TempleCloud <tim.langford@gmail.com>
2014-09-07 13:57:18 +0100

    Working on ch11 manning otp in action. OTP implementation. Added basic 'gws_connection_sup' behaviour.

---
+++ erlang/erlang-otp-in-action/ch11.2-http-server/sc_release/gen_web_server/src/gws_connection_sup.erl
@@ -0,0 +1,103 @@
+%%%============================================================================
+%%% @doc 
+%%% The supervisor implemented by the gws_connection_sup module will be a 
+%%% 'simple-one-for-one' supervisor, used as a factory for handler processes.
+%%% @end
+%%%============================================================================
+-module(gws_connection_sup).
+
+%%%============================================================================
+%%% Public Init API Interface
+%%%============================================================================
+
+-export([start_link/4, start_child/1]).
+
+
+%%%============================================================================
+%%% OTP Supervisor Behaviour
+%%%============================================================================
+
+-behaviour(supervisor).
+
+-export([init/1]).
+
+
+
+%%%============================================================================
+%%% Public Init API Implementation
+%%%============================================================================
+
+%% Start the 'gen_connection_sup' instance and also kicks off the first 
+%% gws_server child process as soon as the supervisor is up and running. 
+%% 
+%% We could also place the responsibility for doing this in 
+%% gen_web_server:start_link/4, but this way it’s guaranteed that a new 
+%% 'gws_connection_sup process' will always have a process that is listening 
+%% on the socket, ready to handle any incoming connection.
+start_link(Callback, IP, Port, UserArgs) ->
+  % Use the superviser module to start a new 'gws_connection_sup' process.
+  {ok, Pid} = supervisor:start_link(?MODULE, [Callback, IP, Port, UserArgs]),
+  % User the new superviser to start an initial 'gws_server' chld process.
+  start_child(Pid),
+  {ok, Pid}.
+
+
+%% Start a new 'gws_server' child process. 
+%% 
+start_child(Server) ->
+  supervisor:start_child(Server, []).
+
+
+
+%%%============================================================================
+%%% OTP Supervisor Behaviour Implementation
+%%%============================================================================ 
+
+init([Callback, IP, Port, UserArgs]) ->
+
+  % Define the BasicSockOpts.
+  %
+  BasicSockOpts = [
+    binary,
+    {active, false},
+    {packet, http_bin},
+    {reuseaddr, true}
+    ],
+  
+  % If an IP is defined then add it to the BasicSockOpts.
+  %
+  SockOpts = 
+    case IP of
+      undefined -> BasicSockOpts;
+      _        -> [{ip,IP} | BasicSockOpts]
+    end,
+    
+  % Open a new listening Socket.
+  %
+  {ok, LSock} = gen_tcp:listen(Port, SockOpts),
+
+  % Define the 'gws_connection_sup' init process.
+  %
+  Server = {                            % The Id to identify the child specification.
+    gws_server, {                       % The apply(M, F, A) tuple to start the process.
+    gws_server, 
+      start_link, 
+      [Callback, LSock, UserArgs]
+    },
+    temporary,                          % Child process always restarted.
+    brutal_kill,                        % Terminate child: immediately
+    worker,                             % Child is a worker process.
+    [gws_server]                        % The name of the callback module.
+    },
+
+    RestartStrategy = {simple_one_for_one, 1000, 3600},
+    
+    {ok, {RestartStrategy, [Server]}}.
+
+
+
+
+
+
+
+
