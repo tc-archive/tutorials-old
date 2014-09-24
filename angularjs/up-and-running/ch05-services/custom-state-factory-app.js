f354d09b6490df4c75c0dd0a23d37a5c72f77b75
TempleCloud <tim.langford@gmail.com>
2014-09-24 22:47:16 +0100

    AngularJS Up and Running. Ch5 Examples.

--- angularjs/up-and-running/ch05-services/custom-state-factory-app.js
+++ angularjs/up-and-running/ch05-services/custom-state-factory-app.js
@@ -1,28 +1,3 @@
-  // Service Function
-  //
-  // Service definition function is now a JavaScript class function. It doesn’t 
-  // return anything.
-  //
-  // The list is private (decalred as a 'var')
-  //
-  // The service publically expose two interface methods:
-  // 1) list (getter).
-  // 2) add (mutator)
-  //
-  //
-function ItemService() {
-  var items = [
-    {id: 1, label: 'Item 0'},
-    {id: 2, label: 'Item 1'}
-    ];
-  this.list = function() {
-    return items;
-  };
-  this.add = function(item) {
-    items.push(item);
-    };
-}
-
 /**
  * The ItemService gets instantiated once when the application loads and the SubCtrl is 
  * loaded, at which point AngularJS decides it needs an instance of the ItemService.
@@ -30,22 +5,24 @@ function ItemService() {
  * After it is created, all other controllers that ask for the ItemService will get the 
  * exact same instance that was returned the very first time.
  */
- angular.module('customStateServiceApp', []).
-
-  service('ItemService', [ItemService]).
-
+angular.module('customStateFactoryApp', []).
+  
   // Main Controller
   //
   // Manage the 'tab' view state.
   //
   controller('MainCtrl', [function() {
+
     var self = this;
-    self.tab = 'first'; 
+    
+    self.tab = 'first';
+    
     self.open = function(tab) {
       self.tab = tab;
-      };
-  }]).
+    };
 
+  }]).
+  
   // SubController
   //
   // Manage the ListService view state,
@@ -55,20 +32,50 @@ function ItemService() {
   // Uses the services exposed interface to add state to and 
   // from the controller.
   //
-  controller('SubCtrl', ['ItemService', function(ItemService) {
-    var self = this;
+  controller('SubCtrl', [
+    'ItemService', function(ItemService) { 
+
+    var self = this; 
+    
     self.list = function() {
       return ItemService.list();
     };
+
     self.add = function() {
       ItemService.add({
         id: self.list().length + 1,
         label: 'Item ' + self.list().length
-    });
-  }; 
+      });
+    };
 
-}]);
+  }]).
 
+  // Service Factory
+  //
+  // This is a factory service that is reponsible for storing the 
+  // list of items.
+  //
+  // The list is private (decalred as a 'var')
+  //
+  // The service publically expose two interface methods:
+  // 1) list (getter).
+  // 2) add (mutator)
+  //
+  //
+  factory('ItemService', [function() {
 
+    var items = [
+      {id: 1, label: 'Item 0'},
+      {id: 2, label: 'Item 1'}
+    ];
 
+    return {
+      list: function() {
+        return items;
+      },
+      add: function(item) {
+        items.push(item);
+      } 
+    };
 
+  }]);
