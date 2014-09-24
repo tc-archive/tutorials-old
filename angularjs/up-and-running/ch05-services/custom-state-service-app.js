d21f4b1f4954312170517c99d4439fe29c5a5580
TempleCloud <tim.langford@gmail.com>
2014-09-24 22:32:56 +0100

    AngularJS Up and Running. Ch5 Examples.

---
+++ angularjs/up-and-running/ch05-services/custom-state-service-app.js
@@ -0,0 +1,82 @@
+/**
+ * The ItemService gets instantiated once when the application loads and the SubCtrl is 
+ * loaded, at which point AngularJS decides it needs an instance of the ItemService.
+ */
+angular.module('notesApp', []).
+  
+  // Main Controller
+  //
+  // Manage the 'tab' view state.
+  //
+  controller('MainCtrl', [function() {
+
+    var self = this;
+    
+    self.tab = 'first';
+    
+    self.open = function(tab) {
+      self.tab = tab;
+    };
+
+  }]).
+  
+  // SubController
+  //
+  // Manage the ListService view state,
+  //
+  // Has the ItemService injected into it upon creation.
+  //
+  // Uses the services exposed interface to add state to and 
+  // from the controller.
+  //
+  controller('SubCtrl', [
+    'ItemService', function(ItemService) { 
+
+    var self = this; 
+    
+    self.list = function() {
+      return ItemService.list();
+    };
+
+    self.add = function() {
+      ItemService.add({
+        id: self.list().length + 1,
+        label: 'Item ' + self.list().length
+      });
+    };
+
+  }]).
+
+  // Service Factory
+  //
+  // This is a factory service that is reponsible for storing the 
+  // list of items.
+  //
+  // The list is private (decalred as a 'var')
+  //
+  // The service publically expose two interface methods:
+  // 1) list (getter).
+  // 2) add (mutator)
+  //
+  //
+  factory('ItemService', [function() {
+
+    var items = [
+      {id: 1, label: 'Item 0'},
+      {id: 2, label: 'Item 1'}
+    ];
+
+    return {
+      list: function() {
+        return items;
+      },
+      add: function(item) {
+        items.push(item);
+      } 
+    };
+
+  }]);
+
+
+
+
