32c5553e90b99a73e4b2d2171920504d8cad4204
TempleCloud <tim.langford@gmail.com>
2014-10-15 15:55:59 +0100

    AngularJS Up and Running. Ch10. Examining FIFA demo app.

--- angularjs/up-and-running/ch10-routing/fifa-teams-app/app/scripts/app.js
+++ angularjs/up-and-running/ch10-routing/fifa-teams-app/app/scripts/app.js
@@ -1,7 +1,18 @@
 // File: chapter10/routing-example/app/scripts/app.js
+
+// We define our application (and create our module) in the app.js file. We 
+// define our 'fifaApp' module, and specify that it depends on the 'ngRoute' 
+// module so that we can use routing in our application.
+//
+// This is where the primary 'router' for the application is defined.
+//
 angular.module('fifaApp', ['ngRoute'])
   .config(function($routeProvider) {
 
+    // The '/' route introduces nothing new, in that it loads a 'templateUrl'  
+    // and attaches the 'TeamListCtrl' to it when it loads. There are no 
+    // variables in the URL, nor any access control checks.
+    //
     $routeProvider.when('/', {
       templateUrl: 'views/team_list.html',
       controller: 'TeamListCtrl as teamListCtrl'
