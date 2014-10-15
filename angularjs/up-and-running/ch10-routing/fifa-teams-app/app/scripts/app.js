7b84e088913fee7fdde0536a3a745dde05249b87
TempleCloud <tim.langford@gmail.com>
2014-10-15 16:12:50 +0100

    AngularJS Up and Running. Ch10. Examining FIFA demo app.

--- angularjs/up-and-running/ch10-routing/fifa-teams-app/app/scripts/app.js
+++ angularjs/up-and-running/ch10-routing/fifa-teams-app/app/scripts/app.js
@@ -6,6 +6,43 @@
 //
 // This is where the primary 'router' for the application is defined.
 //
+// The most complicated route is the 'TeamDetails' route. Here are the 
+// details:
+//
+// • The route itself has the variable code defined in it, which is made 
+//   available to controllers using '$routeParams' service. A controller could 
+//   get the '$routeParams' service injected, and then access 
+//   '$routeParams.code', as our TeamDetailsCtrl does.
+//
+// • The controller is defined in the route, and uses the 'controllerAs' 
+//   syntax to name the instance of the controller teamDetailsCtrl.
+//
+// • We use a resolve object with one key, 'auth'. This key is arbitrary, but 
+//   resolve is used as a way of checking with the server to see if the user is 
+//   currently logged in. The auth key takes a function, using the Dependency 
+//   Injection syntax, so we can inject any services we need into each 
+//   individual resolve key.
+//
+// • The authentication resolve injects UserService into it, and makes a call to 
+//   User Service.session() (which makes a server call to '/api/session'). The 
+//   auth resolve function returns a promise. AngularJS then guarantees the 
+//   following:
+//
+//    — AngularJS won’t load the page until the promise is successfully 
+//      fulfilled.
+//
+//    — AngularJS will prevent the page from loading if the promise fails.
+//
+// • We also chain the promise and do nothing in the case of success. This 
+//   ensures that the returned promise succeeds if the server call succeeds.
+//
+// • We add an error handler in the 'then' of the promise to redirect the user 
+//   to the login screen, in case the server returns a non-200 response. We 
+//   also make sure that we reject the promise in the case of an error, because 
+//   we still want the promise to fail. If we don’t '$q.reject', that tells 
+//   AngularJS that the error was handled successfully.
+//
+//
 angular.module('fifaApp', ['ngRoute'])
   .config(function($routeProvider) {
 
@@ -42,6 +79,8 @@ angular.module('fifaApp', ['ngRoute'])
                function(success) {},
                function(err) {
                   $location.path('/login');
+                  // Prevents the path the user accessed from entering the 
+                  // browser’s history.
                   $location.replace();
                   return $q.reject(err);
              });
