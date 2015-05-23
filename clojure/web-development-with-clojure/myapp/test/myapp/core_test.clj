27e799991411b6c7087b1e92cef8c7db520b2214
TempleCloud <tim.langford@gmail.com>
2015-05-23 15:32:28 +0100

    Small updates.

--- clojure/web-development-with-clojure/myapp/test/myapp/core_test.clj
+++ clojure/web-development-with-clojure/myapp/test/myapp/core_test.clj
@@ -2,6 +2,10 @@
   (:require [clojure.test :refer :all]
             [myapp.core :refer :all]))
 
+;(deftest a-test
+;  (testing "FIXME, I fail."
+;    (is (= 0 1))))
+
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "Simple test."
+    (is (= 1 1))))
